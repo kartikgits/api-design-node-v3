6455bd9982d88d45d02cff19f97f470a92444ec0
Kartikeya <kartik.kapparwan1@gmail.com>
2021-08-21 11:09:58 +0530

    solve exercise-2

--- src/resources/item/item.router.js
+++ src/resources/item/item.router.js
@@ -1,5 +1,22 @@
 import { Router } from 'express'
 
+// mock controller
+const controller = (req, res) => {
+  res.send({ message: 'hello' })
+}
+
 const router = Router()
+
+// /api/item
+router
+  .route(`/`)
+  .get(controller)
+  .post(controller)
+
+router
+  .route(`/:id`)
+  .get(controller)
+  .put(controller)
+  .delete(controller)
 
 export default router
